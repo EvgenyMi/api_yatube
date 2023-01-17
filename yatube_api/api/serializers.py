fa7979d2f1b51922dff099966acc5f84a77edc35
Евгений Минеев <ew.mineew@ya.ru>
2023-01-17 23:47:27 +0800

    Перед первым ревью

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,26 @@
+from posts.models import Comment, Group, Post
+from rest_framework import serializers
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+
+    class Meta:
+        fields = ('id', 'author', 'post', 'text', 'created')
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title', 'slug', 'description')
+        model = Group
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
+        model = Post
